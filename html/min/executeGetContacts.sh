0e6939d6d258bcec1876e731336f7fb6db1fff7f
ksami <ksami.ihide@gmail.com>
2015-09-23 00:29:49 +0800

    echo filename in shell cmd

--- html/min/executeGetContacts.sh
+++ html/min/executeGetContacts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for f in *.html; do python getContactsMin.py $f > ../../data/min/${f%.html}.txt; done
+for f in *.html; do echo $f; python getContactsMin.py $f > ../../data/min/${f%.html}.txt; done
